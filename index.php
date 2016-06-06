4762138cf75b894fd3515e0bf76568b8909499b0
Golam Rabbani <knight3rrantt@gmail.com>
2016-06-06 21:23:51 +0600

    fixes some issues

--- index.php
+++ index.php
@@ -11,25 +11,25 @@
 
 
 		<div class="text">
-		<?php if (isset($_GET['dob']) ): ?>
+		<?php if (!empty($_POST['dob']) ): ?>
 		<?php
-		$datetime1 = new DateTime($_GET['dob']);
+		$datetime1 = new DateTime($_POST['dob']);
 		$datetime2 = new DateTime('now');
 		$interval = $datetime1->diff($datetime2);
 		?>
 
-		<h1>Bravo!</h1>
+		
 		 <h1><?php echo $interval->format('You\'re %y years %m months and %d days old'); ?></h1>
 
 		 <a href="index.php">Check for another?</a>
 		 <?php else: ?>
 			<h1>Enter Your Birth Year</h1>
-			<form action="" method="get">
-				<input class="heda" type="text" name="dob" placeholder="Enter your Birth Year">
+			<form action="" method="post">
+				<input class="f" type="text" name="dob" placeholder="Your Birth Date">
 			<button class="button" type="submit">Submit</button>
 			</form>
 		<?php endif; ?>
 		</div>	
-<a href="age_calculator.zip" style="bottom:20px;right:20px ; color:green;position:absolute">Source Code</a>
+<a href="https://github.com/knight3rrant/age_calculator" style="bottom:20px;right:20px ; color:green;position:absolute">Source Code</a>
 </body>
 </html>
